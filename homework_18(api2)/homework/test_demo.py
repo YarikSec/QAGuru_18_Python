29df4fba76a079d2e92a8904cd4e8941985f8003
Yaroslav Slipchishin <yarik.wade@gmail.com>
2025-04-12 01:14:46 +0300

    Начал 18 задание

---
+++ homework_18(api2)/homework/test_demo.py
@@ -0,0 +1,45 @@
+import requests
+import allure
+from allure_commons._allure import step
+import pytest
+import selene
+from selene import have, browser
+
+
+"""
+1. Написать несколько тестов на demoshop на добавление товаров в корзину через API с проверкой корзины через UI.
+
+2. Автоматизировать у себя в коде логирование в allure
+
+3. Задача со *: реализовать логирование реквеста в аллюр и в консоль https://demowebshop.tricentis.com/
+
+4) Если хочешь, чтобы в логах был красивый читаемый текст вместо абракадабры, то нужно заменить в методе log_to_console строчку
+в HTTP Response Body:
+{result.text}
+на
+{json.dumps(json.loads(result.text), indent=4, ensure_ascii=False) if result.text else "None"}
+"""
+
+@pytest.fixture(scope='function', autouse=True)
+
+
+def test_add_to_cart():
+    # Given
+    """
+    Добавить товар в корзину через API
+    """
+    with allure.step('add to cart'):
+        result = requests.post('https://demowebshop.tricentis.com/addproducttocart/details/38/1')
+        print(result.status_code)
+
+    # When
+    with allure.step('open browser'):
+        browser.open('https://demowebshop.tricentis.com/')
+
+    # Then
+    """
+    Проверить что в корзине 1 товар
+    Проверить что товар в корзине добавлен
+    """
+    with allure.step('check cart'):
+        browser.element('[class="cart-qty"]').should(have.text('1'))
